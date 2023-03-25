0f1d2a1591a6dfb3859ec4daf535c1fdff3f9eb1
SimonDMC <simondmcplayer@gmail.com>
2023-03-25 14:35:10 +0100

    Add persistance

--- chrutils.js
+++ chrutils.js
@@ -1,10 +1,28 @@
+// set sandbox state from storage
+chrome.storage.sync.get("sandboxEnabled", (data) => {
+    toggleSandbox(data.sandboxEnabled);
+});
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     // listen to sandbox toggle message
     if (message.type === "toggleSandbox") {
-        if (message.enabled) {
-            document.body.contentEditable = true;
-        } else {
-            document.body.contentEditable = false;
-        }
+        toggleSandbox(message.enabled);
     }
 });
+
+document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === "visible") {
+        // set sandbox state from storage
+        chrome.storage.sync.get("sandboxEnabled", (data) => {
+            toggleSandbox(data.sandboxEnabled);
+        });
+    }
+});
+
+function toggleSandbox(enabled) {
+    if (enabled) {
+        document.body.contentEditable = true;
+    } else {
+        document.body.contentEditable = false;
+    }
+}
--- popup.js
+++ popup.js
@@ -1,13 +1,21 @@
 // get the checkbox
 const checkBox = document.getElementById("sandbox");
 
+// load state
+chrome.storage.sync.get("sandboxEnabled", (data) => {
+    checkBox.checked = data.sandboxEnabled;
+});
+
 // add event listener
 checkBox.addEventListener("change", (event) => {
-    // send message to background script
+    // send message to content script
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             type: "toggleSandbox",
             enabled: checkBox.checked,
         });
     });
+
+    // save state
+    chrome.storage.sync.set({ sandboxEnabled: checkBox.checked });
 });
