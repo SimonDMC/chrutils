b48c2897ac9b017a843bbffa37b4f85444166fac
SimonDMC <simondmcplayer@gmail.com>
2023-03-25 16:24:02 +0100

    Add sharp corners

--- chrutils.js
+++ chrutils.js
@@ -1,20 +1,38 @@
-// set sandbox state from storage
+// add css to head
+const style = document.createElement("style");
+style.innerHTML = `
+body.sharp-corners * {
+    border-radius: 0 !important;
+}
+`;
+document.head.appendChild(style);
+
+// set states from storage
 chrome.storage.sync.get("sandboxEnabled", (data) => {
     toggleSandbox(data.sandboxEnabled);
 });
+chrome.storage.sync.get("sharpCornersEnabled", (data) => {
+    toggleSharpCorners(data.sharpCornersEnabled);
+});
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    // listen to sandbox toggle message
+    // listen to toggle messages
     if (message.type === "toggleSandbox") {
         toggleSandbox(message.enabled);
+    }
+    if (message.type === "toggleSharpCorners") {
+        toggleSharpCorners(message.enabled);
     }
 });
 
 document.addEventListener("visibilitychange", function () {
     if (document.visibilityState === "visible") {
-        // set sandbox state from storage
+        // set states from storage
         chrome.storage.sync.get("sandboxEnabled", (data) => {
             toggleSandbox(data.sandboxEnabled);
+        });
+        chrome.storage.sync.get("sharpCornersEnabled", (data) => {
+            toggleSharpCorners(data.sharpCornersEnabled);
         });
     }
 });
@@ -25,4 +43,24 @@ function toggleSandbox(enabled) {
     } else {
         document.body.contentEditable = false;
     }
+}
+
+function toggleSharpCorners(enabled) {
+    if (enabled) {
+        document.body.classList.add("sharp-corners");
+    } else {
+        document.body.classList.remove("sharp-corners");
+    }
+}
+
+function getCurrentTabId() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            if (tabs.length > 0) {
+                resolve(tabs[0].id);
+            } else {
+                reject(new Error("No active tab found"));
+            }
+        });
+    });
 }
--- popup.js
+++ popup.js
@@ -1,21 +1,42 @@
-// get the checkbox
-const checkBox = document.getElementById("sandbox");
+const toggles = [
+    {
+        label: "sandbox",
+        element: document.getElementById("sandbox"),
+        enabledLabel: "sandboxEnabled",
+        toggleLabel: "toggleSandbox",
+    },
+    {
+        label: "sharp-corners",
+        element: document.getElementById("sharp-corners"),
+        enabledLabel: "sharpCornersEnabled",
+        toggleLabel: "toggleSharpCorners",
+    },
+];
 
-// load state
-chrome.storage.sync.get("sandboxEnabled", (data) => {
-    checkBox.checked = data.sandboxEnabled;
+// load states
+toggles.forEach((toggle) => {
+    chrome.storage.sync.get(toggle.enabledLabel, (data) => {
+        toggle.element.checked = data[toggle.enabledLabel];
+    });
 });
 
-// add event listener
-checkBox.addEventListener("change", (event) => {
-    // send message to content script
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            type: "toggleSandbox",
-            enabled: checkBox.checked,
+// add event listeners
+toggles.forEach((toggle) => {
+    toggle.element.addEventListener("change", (event) => {
+        // send message to content script
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    type: toggle.toggleLabel,
+                    enabled: toggle.element.checked,
+                });
+            }
+        );
+
+        // save state
+        chrome.storage.sync.set({
+            [toggle.enabledLabel]: toggle.element.checked,
         });
     });
-
-    // save state
-    chrome.storage.sync.set({ sandboxEnabled: checkBox.checked });
 });
